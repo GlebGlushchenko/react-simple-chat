2b980f9f0687c8bda867a31a9e06b89abcae4abd
GlebGlushchenko <Darkvee1n@yandex.ru>
2021-03-29 22:47:42 +0300

    add socket io

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 <li>React js</li>
 <li>Node js</li>
 <li>Express js</li>
+<li>Socket.io</li>
+<li>Socket.io-client</li>
 </ul>
--- server.js
+++ server.js
@@ -1,15 +1,33 @@
 const express = require('express')
+const useSocket = require('socket.io')
 
 const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+    credential: true,
+    transports: ['websocket'],
+  },
+})
 
-const rooms = {
-  rooms: [],
-  messages: ['Hello'],
-}
+const rooms = new Map([
+  ['room', []],
+  ['messages', []],
+])
 
-app.get('/users', function (req, res) {
-  console.log('Hello1s')
+app.get('/rooms', (req, res) => {
   res.json(rooms)
 })
 
-app.listen(9999)
+io.on('connection', (socket) => {
+  console.log('socket connected', socket.id)
+})
+
+server.listen(9999, (err) => {
+  if (err) {
+    throw Error(err)
+  }
+  console.log('Server started')
+})
